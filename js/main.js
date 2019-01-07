f776009fae440f6c9203f83a65825a43ed9a06b5
Artem <35493226+ArtemDrushchyts@users.noreply.github.com>
2019-01-07 22:53:16 +0300

    test project

---
+++ js/main.js
@@ -0,0 +1,10 @@
+ $(document).ready(function(){
+      $('.advantages__slaider').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        autoplay: true,
+        autoplaySpeed: 3000
+      });
+    });
