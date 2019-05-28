1673c553fbebaf59d3b7475dcd96fc046af669ef
Artsiom Drushchyts <temafm-3@yandex.ru>
2019-05-28 22:40:29 +0300

    refactor nav, adaptive nav

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
- $(document).ready(function(){
-      $('.advantages__slaider').slick({
-        dots: true,
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        autoplay: true,
-        autoplaySpeed: 3000
-      });
-    });
+$(document).ready(function () {
+  $('.advantages__slaider').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 5000
+  });
+});
