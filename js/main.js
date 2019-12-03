3d024778980ca118fb64023dc137ec2c24e14be6
Артем Друщиц <temafm-3@yandex.ru>
2019-12-03 23:55:08 +0300

    feat: add modal animation

--- js/main.js
+++ js/main.js
@@ -9,3 +9,25 @@ $(document).ready(function () {
   });
 });
 
+window.addEventListener('DOMContentLoaded', function() {
+  let mainBtn = document.getElementsByClassName('main-btn')[0];
+  let overlay = document.getElementsByClassName('overlay')[0];
+  let close = document.getElementsByClassName('popup-close')[0];
+  let popup = document.getElementsByClassName('popup')[0];
+
+  mainBtn.addEventListener('click', () => {
+
+    overlay.style.display = 'block';
+    overlay.classList.add('animateSlade');
+    document.body.style.overflow = 'hidden';
+
+  });
+
+  close.addEventListener('click', () => {
+    overlay.classList.remove('animateSlade');
+    overlay.style.display = 'none';
+    document.body.style.overflow = '';
+
+  })
+
+});
